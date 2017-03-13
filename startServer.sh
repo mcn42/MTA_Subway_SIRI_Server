6f0d79ddb82c66c19d3e3defca957a16f7be6f4d
mcn42 <michael.nilsen@gmail.com>
2017-03-13 13:12:21 +0000

    sync

--- startServer.sh
+++ startServer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cd /home/ubuntu/code/MTA_Subway_SIRI_Server
-. ./setAwsTags.sh
-node siri_server.js
+. ./import-tags.sh
+. ./modifyJson.sh
+echo Server config: 
+cat config/server.json
+node ./siri_server.js
